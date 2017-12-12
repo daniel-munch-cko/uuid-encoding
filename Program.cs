34d1956f1d9b01f987e0a30891de73c47a0b47cf
Daniel Münch <daniel.munch@checkout.com>
2017-12-12 11:19:34 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uuid_encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uid = Guid.NewGuid();
+            var uidBytes = uid.ToByteArray();
+            
+            Console.WriteLine($"base16: {uid:n}");
+            Console.WriteLine($"base32: {Base32.Encode(uidBytes).ToLowerInvariant()}");
+            Console.WriteLine($"base64: {Convert.ToBase64String(uidBytes)}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# uuid encoding
+
+Shows 3 differents ways of encoding a .NET `Guid`.
+
+```console
+base16: fc47beff2a09426fa6beaa7d52c4ce74
+base32: 767ep7ajfjxufjv6vj6vfrgooq
+base64: /75H/Akqb0Kmvqp9UsTOdA==
+```
