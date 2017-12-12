33bb09766ded09b40079b1e7613340566352d57f
Daniel Münch <daniel.munch@checkout.com>
2017-12-12 15:35:43 +0100

    Add example to convert back from base32 and base64 to Guid.

--- Program.cs
+++ Program.cs
@@ -9,9 +9,24 @@ namespace uuid_encoding
             var uid = Guid.NewGuid();
             var uidBytes = uid.ToByteArray();
             
+            var base32 = Base32.Encode(uidBytes).ToLowerInvariant();
+            var base64 = Convert.ToBase64String(uidBytes);
+
             Console.WriteLine($"base16: {uid:n}");
             Console.WriteLine($"base32: {Base32.Encode(uidBytes).ToLowerInvariant()}");
             Console.WriteLine($"base64: {Convert.ToBase64String(uidBytes)}");
+
+            
+            var fromBase32 = new Guid(Base32.Decode(base32));
+            var fromBase64 = new Guid(Convert.FromBase64String(base64));
+
+            Console.WriteLine();
+            Console.WriteLine($"====== And back ======");
+            Console.WriteLine();
+            
+            Console.WriteLine($"original    : {uid:n}");
+            Console.WriteLine($"from base32 : {fromBase32:n}");
+            Console.WriteLine($"from base64 : {fromBase64:n}");
         }
     }
 }
--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 Shows 3 differents ways of encoding a .NET `Guid`.
 
 ```console
-base16: fc47beff2a09426fa6beaa7d52c4ce74
-base32: 767ep7ajfjxufjv6vj6vfrgooq
-base64: /75H/Akqb0Kmvqp9UsTOdA==
+base16: 8685662772b74aa7b48aa948264fa830
+base32: e5tilbvxoktuvnekvfecmt5iga
+base64: J2aFhrdyp0q0iqlIJk+oMA==
+
+====== And back ======
+
+original    : 8685662772b74aa7b48aa948264fa830
+from base32 : 8685662772b74aa7b48aa948264fa830
+from base64 : 8685662772b74aa7b48aa948264fa830
 ```
