c8262a2a77ca020943faf6d4c49950fea6a4cd28
Daniel Münch <daniel.munch@checkout.com>
2017-12-13 16:00:56 +0100

    Add zbase32 example.

--- Program.cs
+++ Program.cs
@@ -10,19 +10,27 @@ namespace uuid_encoding
             var uidBytes = uid.ToByteArray();
             
             var base32 = Base32.Encode(uidBytes).ToLowerInvariant();
+            var zbase32 = ZBase32Encoder.ZBase32Encoder.Encode(uidBytes).ToLowerInvariant();
             var base64 = Convert.ToBase64String(uidBytes);
 
-            Console.WriteLine($"base16: {uid:n}");
-            Console.WriteLine($"base32: {Base32.Encode(uidBytes).ToLowerInvariant()}");
-            Console.WriteLine($"base64: {Convert.ToBase64String(uidBytes)}");
+            Console.WriteLine($"base16 : {uid:n}");
+            Console.WriteLine($"base32 : {base32}");
+            Console.WriteLine($"zbase32: {zbase32}");
+            Console.WriteLine($"base64 : {base64}");
 
             
             var fromBase32 = new Guid(Base32.Decode(base32));
+            var fromzBase32 = new Guid(ZBase32Encoder.ZBase32Encoder.Decode(zbase32));
             var fromBase64 = new Guid(Convert.FromBase64String(base64));
 
             Console.WriteLine();
             Console.WriteLine($"====== And back ======");
             Console.WriteLine();
+
+            Console.WriteLine($"original     : {uid:n}");
+            Console.WriteLine($"from base32  : {fromBase32:n}");
+            Console.WriteLine($"from zbase32 : {fromzBase32:n}");
+            Console.WriteLine($"from base64  : {fromBase64:n}");
             
             Console.WriteLine($"original    : {uid:n}");
             Console.WriteLine($"from base32 : {fromBase32:n}");
--- README.md
+++ README.md
@@ -3,13 +3,15 @@
 Shows 3 differents ways of encoding a .NET `Guid`.
 
 ```console
-base16: 8685662772b74aa7b48aa948264fa830
-base32: e5tilbvxoktuvnekvfecmt5iga
-base64: J2aFhrdyp0q0iqlIJk+oMA==
+base16 : 9703ddc67e9e4b33a4226e8c8041a1e7
+base32 : y3oqhf46pyzuxjbcn2giaqnb44
+zbase32: a5qo8fh6xa3wzjbnp4geyopbhh
+base64 : xt0Dl55+M0ukIm6MgEGh5w==
 
 ====== And back ======
 
-original    : 8685662772b74aa7b48aa948264fa830
-from base32 : 8685662772b74aa7b48aa948264fa830
-from base64 : 8685662772b74aa7b48aa948264fa830
+original     : 9703ddc67e9e4b33a4226e8c8041a1e7
+from base32  : 9703ddc67e9e4b33a4226e8c8041a1e7
+from zbase32 : 9703ddc67e9e4b33a4226e8c8041a1e7
+from base64  : 9703ddc67e9e4b33a4226e8c8041a1e7
 ```
