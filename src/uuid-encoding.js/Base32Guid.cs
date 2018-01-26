c6555bc37ca77e0364ba6c76ad5b2dcae999d334
Daniel Münch <daniel.munch@checkout.com>
2018-01-26 16:43:34 +0100

    Add uuid-encoding.AMD, a tiny-winy solution for browsers

--- src/uuid-encoding.js/Base32Guid.cs
+++ src/uuid-encoding.js/Base32Guid.cs
@@ -8,4 +8,10 @@ public class Base32Guid
     {
         return Base32.Encode(Guid.NewGuid().ToByteArray());
     }
+
+    public static string Decode(string base32Guid)
+    {
+        var guid = new Guid(Base32.Decode(base32Guid));
+        return guid.ToString();
+    }
 }
