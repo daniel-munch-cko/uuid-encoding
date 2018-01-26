fc2b4bcdbdaf185ade1afe4e5c6f5795108f2616
Daniel Münch <daniel.munch@checkout.com>
2018-01-26 12:05:19 +0100

    Restructure src folder and create uuid-encoding.js, project compiling the Base32 encoder to javascript

---
+++ src/uuid-encoding.js/Base32Guid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uuid_encoding
+{
+    public class Base32Guid
+    {
+        public static string Create()
+        {
+            return Base32.Encode(Guid.NewGuid().ToByteArray());
+        }
+    }
+}
